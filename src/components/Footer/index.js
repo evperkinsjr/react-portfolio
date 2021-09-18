7d127d88b975209a5d3bd599ae3f46af44d74aa4
Enrico Perkins Jr <evperkinsjr@gmail.com>
2021-09-18 18:08:10 -0400

    added contact info and links to footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,1 +1,14 @@
 import React from 'react';
+
+function Footer() {
+    return (
+        <address>
+            <a href="tel:980.292.0576">980.292.0576</a>
+            <a href="mailto:enrico.perkins@gmail.com">enrico.perkins@gmail.com</a>
+            <a href="https://www.linkedin.com/in/enricoperkinsjr/">LinkedIn</a>
+            <a href="https://github.com/evperkinsjr">Github</a>
+        </address>
+    );
+}
+
+export default Footer;
