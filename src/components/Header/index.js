3186211589523efc5aeaa18063ab76a9ba0c42aa
Enrico Perkins Jr <evperkinsjr@gmail.com>
2021-09-18 19:19:03 -0400

    added navigation links

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,1 +1,28 @@
 import React from 'react';
+import {Link} from "react-router-dom";
+
+function Navbar() {
+    return (
+        <nav>
+            <Link to="/">Enrico Perkins, Jr.</Link>
+            <div>
+                <ul>
+                    <li>
+                        <Link to="/about">About</Link>
+                    </li>
+                    <li>
+                        <Link to="/projects">Projects</Link>
+                    </li>
+                    <li>
+                        <Link to="/contact">Contact Me</Link>
+                    </li>
+                    <li>
+                        <Link to="https://evperkinsjr.github.io/portfolio-website/assets/documents/Enrico_Perkins_Resume.pdf">Resume</Link>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
