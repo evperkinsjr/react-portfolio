efe92e52ef62439db6706812c81d44fa7e167ae8
Enrico Perkins Jr <evperkinsjr@gmail.com>
2021-09-18 22:46:41 -0400

    added and exported about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,1 +1,12 @@
-import React from 'react';
+import React from "react";
+import About from "../components/About";
+
+function AboutPage() {
+    return (
+        <div>
+            <About />
+        </div>
+    );
+}
+
+export default AboutPage;
