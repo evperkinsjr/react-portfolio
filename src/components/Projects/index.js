2cd4f19297ad0a9aa44a0008395b887afdfb3c13
Enrico Perkins Jr <evperkinsjr@gmail.com>
2021-09-19 13:38:44 -0400

    styling the project cards

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,17 +1,18 @@
 import React from "react";
 import projectsList from "../../utils/projects.json";
+import "./style.css";
 
 function Projects() {
     return (
-        <div>
+        <div className="projects-container">
             {projectsList.map((project) => {
             return (
-                <div className="card">
+                <div className="card flex-project">
                     <img src={project.image} className="card-img-top" alt={project.title}/>
                     <div className="card-body">
                         <h5 className="card-title">{project.title}</h5>
                         <p className="card-text">{project.description}</p>
-                        <p className="card-text">{project.technologies}</p>
+                        <p className="card-text"><b>Technologies</b>: {project.technologies}</p>
                         <a href={project.link} className="card-link">Live Application</a>
                         <a href={project.repo} className="card-link">Repository</a>
                     </div>
