ea10278baaec38d28929e2b18d6cb3c3fddac6b2
Enrico Perkins Jr <evperkinsjr@gmail.com>
2021-09-18 22:58:31 -0400

    added pages and routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Navbar from "./components/Header";
+import AboutPage from "./pages/About";
+import ProjectsPage from "./pages/Projects";
 import Footer from "./components/Footer";
 
 function App() {
   return (
     <Router>
       <Navbar />
+      <Route exact path="/" component={AboutPage} />
+      <Route exact path="/about" component={AboutPage} />
+      <Route exact path="/projects" component={ProjectsPage} />
       <Footer />
     </Router>
   );
