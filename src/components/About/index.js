afb9516a7abd5a90fe75da66291d3967ac3da0d1
Enrico Perkins Jr <evperkinsjr@gmail.com>
2021-09-19 10:40:17 -0400

    added profile photo to about section

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -4,11 +4,14 @@ function About() {
     return (
         <div>
             <h1>About Me</h1>
-            <p>Hi, I'm Enrico. I joined the UNC-Charlotte Coding Bootcamp in April 2021.</p>
-            <p>I'm a digital marketer developing skills in full-stack web development.</p>
-            <p>My extensive background in digital marketing helps me understand how users interact with websites and applications.</p>
-            <p>Strengths include problem solving, creativity, and executing projects independently or collaboratively.</p>
-            <p>I'm seeking to leverage my analytical, creative, and technical abilities to build meaningful applications.</p>
+            <div>
+                <img src="../../images/profile-photo.jpg" alt="Enrico Perkins Jr"/>
+                <p>Hi, I'm Enrico. I joined the UNC-Charlotte Coding Bootcamp in April 2021.</p>
+                <p>I'm a digital marketer developing skills in full-stack web development.</p>
+                <p>My extensive background in digital marketing helps me understand how users interact with websites and applications.</p>
+                <p>Strengths include problem solving, creativity, and executing projects independently or collaboratively.</p>
+                <p>I'm seeking to leverage my analytical, creative, and technical abilities to build meaningful applications.</p>
+            </div>
         </div>
     );
 }
