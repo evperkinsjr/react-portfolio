47b4e34510cff83825ddeb6f3d39b705454df0ec
Enrico Perkins Jr <evperkinsjr@gmail.com>
2021-09-19 13:09:19 -0400

    added wrapper component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Navbar from "./components/Header";
+import Wrapper from "./components/Wrapper";
 import AboutPage from "./pages/About";
 import ProjectsPage from "./pages/Projects";
 import Footer from "./components/Footer";
@@ -10,9 +11,11 @@ function App() {
   return (
     <Router>
       <Navbar />
+      <Wrapper>
       <Route exact path="/" component={AboutPage} />
       <Route exact path="/about" component={AboutPage} />
       <Route exact path="/projects" component={ProjectsPage} />
+      </Wrapper>
       <Footer />
     </Router>
   );
