e76a6bf273939a0902393b4c1e27604d5ab2b69e
Enrico Perkins Jr <evperkinsjr@gmail.com>
2021-10-13 16:50:05 -0400

    adding new files for redesign with tailwind css and netlify

---
+++ src/data.js
@@ -0,0 +1,71 @@
+export const projects = [
+  {
+    title: "",
+    subtitle: "",
+    description: "",
+    image: "",
+    link: "",
+  },
+  {
+    title: "",
+    subtitle: "",
+    description: "",
+    image: "",
+    link: "",
+  },
+  {
+    title: "",
+    subtitle: "",
+    description: "",
+    image: "",
+    link: "",
+  },
+  {
+    title: "",
+    subtitle: "",
+    description: "",
+    image: "",
+    link: "",
+  },
+  {
+    title: "",
+    subtitle: "",
+    description: "",
+    image: "",
+    link: "",
+  },
+  {
+    title: "",
+    subtitle: "",
+    description: "",
+    image: "",
+    link: "",
+  },
+];
+
+export const testimonials = [
+  {
+    quote: "",
+    image: "",
+    name: "",
+    company: "",
+  },
+  {
+    quote: "",
+    image: "",
+    name: "",
+    company: "",
+  },
+];
+
+export const skills = [
+  "JavaScript",
+  "MongoDB",
+  "Express",
+  "React",
+  "Node",
+  "Mongoose",
+  "MySQL",
+  "Sequelize",
+  "CSS Frameworks"
+];
