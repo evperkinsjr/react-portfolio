d4d1c5217305861219c6e713d43843ec6e5250c9
Enrico Perkins Jr <evperkinsjr@gmail.com>
2021-10-27 18:01:56 -0400

    added github and linkedin icons to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,16 @@
 import React from "react";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
 
 export default function Footer() {
   return (
     <footer className="bg-gray-800 bottom-0 z-10">
       <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
         <address className="mx-auto flex flex-wrap items-center text-base justify-center">
-          <a href="https://github.com/evperkinsjr" className="mr-5 hover:text-white">
-            Github
+          <a href="https://github.com/evperkinsjr" className="mx-6 hover:text-white">
+            <FaGithub size="40"/>
           </a>
-          <a href="https://www.linkedin.com/in/enricoperkinsjr/" className="mr-5 hover:text-white">
-            LinkedIn
+          <a href="https://www.linkedin.com/in/enricoperkinsjr/" className="mx-6 hover:text-white">
+            <FaLinkedin size="40"/>
           </a>
         </address>
       </div>
